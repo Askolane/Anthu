53e8ba9ce0c4c971792a9a109d9ca14ca67f5a0a
Lain <90332352+Askolane@users.noreply.github.com>
2026-03-19 13:14:30 +0900

    Add files via upload

---
+++ examples/decay.asm
@@ -0,0 +1,11 @@
+# channel sig → cell 0
++72
+.
+!8
+.
+~8
+.
+# emit string
+>1
++10 .
+-10
---
+++ examples/echo.asm
@@ -0,0 +1,29 @@
+# channel input → cell 0
+# channel display → cell 1
+,
+# copy input → display
+[
+-
+>1
++
+>1
++
+<2
+]
+>2
+[
+-
+<2
++
+>2
+]
+<1
+.
+!3
+.
+~3
+.
+# emit string
+>2
++10 .
+-10
